70ab6bc4f5013ee1800851c36fc2db50080e6ea6
bleasdal3 <49682020+bleasdal3@users.noreply.github.com>
2019-08-08 16:20:08 +0100

    More minor bug fixes

--- DateCriteriaData.cs
+++ DateCriteriaData.cs
@@ -8,6 +8,13 @@ namespace AscendQueryEngine
 {
     public static class DateCriteriaData
     {
+        /* A word about this class - 
+         * I need a way to pass information between the two windows.
+         * I could mess about creating my own event handler 
+         * and subscribe the button to the other windows event...
+         * or... I could just use this one small class as a repository
+         * and wipe it after use. \_Ò.Ó_/
+         */
         public static string ColumnName { get; set; }
         public static string StartDate {get; set;}
 
